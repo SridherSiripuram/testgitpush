f24d48b6a8f4b863dc61e4cbf8bbd017fc47ba47
Sridher Siripuram <sridhar519863@gmail.com>
2022-07-17 19:28:28 +0530

    This is my second commit to push in git

--- mongotest.py
+++ mongotest.py
@@ -10,6 +10,12 @@ d1 ={
     "suname": "siripuram1"
 }
 
+d2 ={
+    "name":"Sridher1",
+    "email": "sridhar@gmail.com",
+    "suname": "siripuram1"
+}
+
 db1 = client['mongotest']
 coll = db1['test']
 coll.insert_one(d1)
